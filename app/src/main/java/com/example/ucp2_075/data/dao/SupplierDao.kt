b43952878d2acfb672a917df7a034b2b44d05c74
Muhammadalif1213 <muhammadalif1213@gmail.com>
2024-12-18 14:34:53 +0700

    membuat fungsi query Creat dan Read di SupplierDAO

---
+++ app/src/main/java/com/example/ucp2_075/data/dao/SupplierDao.kt
@@ -0,0 +1,22 @@
+package com.example.ucp2_075.data.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.ucp2_075.data.entity.Supplier
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface SupplierDao {
+    @Insert
+    suspend fun insertSupplier(
+        supplier: Supplier
+    )
+
+    @Query("SELECT * FROM supplier ORDER BY namaSup ASC")
+    fun getAllSupplier() : Flow<List<Supplier>>
+
+    @Query("SELECT * FROM supplier WHERE id_sup = :id_sup")
+    fun getSupplier(id_sup: String): Flow<Supplier>
+
+}
