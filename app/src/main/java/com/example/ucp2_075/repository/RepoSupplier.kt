7bc74a1016363e21638ad126c20922092e080cfb
Muhammadalif1213 <muhammadalif1213@gmail.com>
2024-12-19 12:35:32 +0700

    membuat repository dari entitas supplier

---
+++ app/src/main/java/com/example/ucp2_075/repository/RepoSupplier.kt
@@ -0,0 +1,12 @@
+package com.example.ucp2_075.repository
+
+import com.example.ucp2_075.data.entity.Supplier
+import kotlinx.coroutines.flow.Flow
+
+interface RepoSupplier {
+    suspend fun insertSup(supplier: Supplier)
+
+    fun getAllSup() : Flow<List<Supplier>>
+
+    fun getSup(id_sup: String): Flow<Supplier>
+}
