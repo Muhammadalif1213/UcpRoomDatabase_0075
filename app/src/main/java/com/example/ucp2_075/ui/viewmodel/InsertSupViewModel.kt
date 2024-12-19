06b276fdf500eb977b70d557056e2780a0ea3efe
Muhammadalif1213 <muhammadalif1213@gmail.com>
2024-12-19 17:40:00 +0700

    membuat fungsi untuk memasukan data ke entitas supplier

--- app/src/main/java/com/example/ucp2_075/ui/viewmodel/InsertSupViewModel.kt
+++ app/src/main/java/com/example/ucp2_075/ui/viewmodel/InsertSupViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.ucp2_075.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.example.ucp2_075.data.entity.Supplier
 import com.example.ucp2_075.repository.RepoSup
 
 class InsertSupViewModel (private val repoSup: RepoSup) : ViewModel(
@@ -10,6 +11,13 @@ class InsertSupViewModel (private val repoSup: RepoSup) : ViewModel(
 }
 
 
+
+fun SupplierEvent.toSupplierEntity(): Supplier = Supplier(
+    id_sup = id_sup,
+    namaSup = namaSup,
+    kontakSup = kontakSup,
+    alamatSup = alamatSup
+)
 
 data class SupplierEvent(
     val id_sup: String = "",
