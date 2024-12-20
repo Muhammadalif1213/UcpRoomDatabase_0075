f55ffe8fcb6350e477b03c80a2f0f0edd697b12f
Muhammadalif1213 <muhammadalif1213@gmail.com>
2024-12-20 17:56:45 +0700

    membuat fungsi untuk memasukan data ke entitas barang

--- app/src/main/java/com/example/ucp2_075/ui/viewmodel/InsertBrgViewModel.kt
+++ app/src/main/java/com/example/ucp2_075/ui/viewmodel/InsertBrgViewModel.kt
@@ -1,7 +1,16 @@
 package com.example.ucp2_075.ui.viewmodel
 
+import com.example.ucp2_075.data.entity.Barang
 
 
+fun BarangEvent.toBarangEntity(): Barang = Barang(
+    id_barang = id_barang,
+    nama = nama,
+    deskripsi = deskripsi,
+    harga = harga,
+    stok = stok,
+    nama_sup = nama_sup
+)
 
 data class BarangEvent(
     val id_barang: String = "" ,
