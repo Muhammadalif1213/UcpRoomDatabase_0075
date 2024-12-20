4e883d421ed51bff85a5592bceb2d35960133fb2
Muhammadalif1213 <muhammadalif1213@gmail.com>
2024-12-20 17:42:26 +0700

    membuat data class BarangEvent untuk menyimpan data input form

---
+++ app/src/main/java/com/example/ucp2_075/ui/viewmodel/InsertBrgViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.ucp2_075.ui.viewmodel
+
+
+
+
+data class BarangEvent(
+    val id_barang: String = "" ,
+    val nama: String = "",
+    val deskripsi: String = "",
+    val harga: String = "",
+    val stok: String = "",
+    val nama_sup: String = "",
+)
