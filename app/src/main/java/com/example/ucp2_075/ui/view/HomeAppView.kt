0035bab620853a3d47f3a91c8e255ed1bdc178c2
Muhammadalif1213 <muhammadalif1213@gmail.com>
2024-12-20 15:31:35 +0700

    membuat fungsi untuk mengatur body dari halaman home dan mengatur card click nya

--- app/src/main/java/com/example/ucp2_075/ui/view/HomeAppView.kt
+++ app/src/main/java/com/example/ucp2_075/ui/view/HomeAppView.kt
@@ -1,7 +1,10 @@
 package com.example.ucp2_075.ui.view
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,6 +14,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -19,14 +23,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.ucp2_075.ui.customWidget.HomeTopBar
 
-@Preview(showBackground = true)
+
 @Composable
 fun HomeAppView(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    onNavigateAddSup: () -> Unit
 ){
     Scaffold (
         modifier = modifier,
@@ -35,43 +43,93 @@ fun HomeAppView(
         }
     ){ innerPadding ->
 
-        CardMenu(
-            modifier = Modifier.padding(innerPadding)
+        BodyHome(
+            modifier = Modifier.padding(innerPadding),
+            onBrgListClick ={
+
+            },
+            onAddSupClick = {
+                onNavigateAddSup()
+            }
         )
     }
 }
 
 
+@Composable
+fun BodyHome(
+    modifier: Modifier = Modifier,
+    onBrgListClick: () -> Unit,
+    onAddSupClick: () -> Unit
+){
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.fillMaxWidth().padding(16.dp)
+    ){
+        Row (
+        ){
+            CardMenu(
+                namaMenu = "List Produk",
+                onClick = onBrgListClick
+            )
+            CardMenu(
+                namaMenu = "Add Product"
+            )
+        }
+        Row(
+        ) {
+            CardMenu(
+                namaMenu = "List Supplier"
+            )
+            CardMenu(
+                namaMenu = "AddSupplier",
+                onClick = onAddSupClick
+            )
+        }
+
+    }
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CardMenu(
+    namaMenu: String,
     modifier: Modifier = Modifier,
     onClick: () -> Unit = {}
-){
-  Card (
-      onClick = onClick,
-      modifier = modifier
-          .width(300.dp)
-          .height(400.dp)
-          .padding(8.dp),
-
-  ){
-      Column(
-          verticalArrangement = Arrangement.Center,
-          horizontalAlignment = Alignment.CenterHorizontally,
-          modifier = Modifier
-              .padding(16.dp)
-              .fillMaxWidth()
-              .fillMaxHeight()
-
-      ) {
-          Icon(imageVector = Icons.Filled.Menu,
-              contentDescription = "")
-          Spacer(modifier = Modifier.padding(8.dp))
-          Text(
-              text = "List Produk"
-          )
-      }
-  }
+) {
+    Card(
+        onClick = onClick,
+        modifier = modifier
+            .width(180.dp)
+            .height(230.dp)
+            .padding(8.dp),
+    ) {
+        Box(
+            modifier = Modifier
+                .background(color = Color(0xFF42A5F5)) // Cyan color
+                .fillMaxHeight()
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Menu,
+                    contentDescription = "",
+                    tint = Color.White,
+                )
+                Spacer(modifier = Modifier.padding(8.dp))
+                Text(
+                    text = namaMenu,
+                    color = Color.White,
+                    fontWeight = FontWeight.ExtraBold
+                )
+            }
+        }
+    }
 }
