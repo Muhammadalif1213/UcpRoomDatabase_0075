905c1b89df528bbaa8400ebd148a0627941ce39b
Muhammadalif1213 <muhammadalif1213@gmail.com>
2024-12-21 00:05:51 +0700

    Menambahkan data class FormErrorBrgState untuk menangani validasi error pada form barang, lengkap dengan fungsi isValid untuk memeriksa validasi inputan

--- app/src/main/java/com/example/ucp2_075/ui/viewmodel/InsertBrgViewModel.kt
+++ app/src/main/java/com/example/ucp2_075/ui/viewmodel/InsertBrgViewModel.kt
@@ -3,6 +3,24 @@ package com.example.ucp2_075.ui.viewmodel
 import com.example.ucp2_075.data.entity.Barang
 
 
+
+
+data class FormErrorBrgState(
+    val id_barang: String? = null,
+    val nama: String? = null,
+    val deskripsi: String? = null,
+    val harga: String? = null,
+    val stok: String? = null,
+    val nama_sup: String? = null,
+
+    ){
+    fun isValid(): Boolean {
+        return id_barang == null && nama == null && deskripsi == null &&
+                harga == null && stok == null && nama_sup == null
+    }
+}
+
+
 fun BarangEvent.toBarangEntity(): Barang = Barang(
     id_barang = id_barang,
     nama = nama,
