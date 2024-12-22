274dac30209ce66591e5ab0c62aafca59d3e7a37
Muhammadalif1213 <muhammadalif1213@gmail.com>
2024-12-22 21:51:33 +0700

    Menambahkan StateFlow untuk Mengelola State UI di ListBrgViewModel

--- app/src/main/java/com/example/ucp2_075/ui/viewmodel/ListBrgViewModel.kt
+++ app/src/main/java/com/example/ucp2_075/ui/viewmodel/ListBrgViewModel.kt
@@ -1,7 +1,52 @@
 package com.example.ucp2_075.ui.viewmodel
 
-
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2_075.data.entity.Barang
+import com.example.ucp2_075.repository.RepoBrg
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.WhileSubscribed
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+
+class ListBrgViewModel(
+    private val repoBrg: RepoBrg
+) :ViewModel(){
+    val listBrgUiState: StateFlow<ListBrgUIstate> = repoBrg.getAllBrg()
+        .filterNotNull()
+        .map {
+            ListBrgUIstate(
+                listBrg = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(ListBrgUIstate(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                ListBrgUIstate(
+                    isLoading = false,
+                    isError = false,
+                    errorMessage = it.message?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = ListBrgUIstate(
+                isLoading = true
+            )
+        )
+}
 
 data class ListBrgUIstate(
     val listBrg: List<Barang> = listOf(),
