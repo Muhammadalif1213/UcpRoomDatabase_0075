2e89544bf4ba039323a1e9111c8653d837734507
Muhammadalif1213 <muhammadalif1213@gmail.com>
2024-12-22 22:01:38 +0700

    Menambahkan fungsi: BodyListBrgView dan ListBrgView di ListBarangView

--- app/src/main/java/com/example/ucp2_075/ui/view/barang/ListBrgView.kt
+++ app/src/main/java/com/example/ucp2_075/ui/view/barang/ListBrgView.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -14,23 +15,126 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ucp2_075.data.entity.Barang
+import com.example.ucp2_075.data.entity.Supplier
+import com.example.ucp2_075.ui.viewmodel.ListBrgUIstate
+import com.example.ucp2_075.ui.viewmodel.ListBrgViewModel
+import com.example.ucp2_075.ui.viewmodel.PenyediaViewModel
+import com.example.ucp2_075.ui.customWidget.TopAppBar
+import kotlinx.coroutines.launch
+
+@Composable
+fun ListBrgView(
+    onBack: () ->Unit,
+    viewModel: ListBrgViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    modifier: Modifier = Modifier,
+    onDetailClick: (String) -> Unit = { },
+){
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            TopAppBar(
+                judul = "List Barang",
+                showBackButton = true,
+                onBack = onBack,
+                modifier = modifier
+            )
+        }
+    ) {
+        innerPadding ->
+        val listBrgUIstate by viewModel.listBrgUiState.collectAsState()
+
+        BodyListBrgView(
+            listBrgUIstate = listBrgUIstate,
+            onClick = {
+                onDetailClick(it)
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+
+    }
+}
 
 
+
+@Composable
+fun BodyListBrgView(
+    listBrgUIstate: ListBrgUIstate,
+    onClick: (String) -> Unit = {},
+    modifier: Modifier = Modifier
+){
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+    when{
+        listBrgUIstate.isLoading ->{
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                CircularProgressIndicator()
+            }
+        }
+        listBrgUIstate.isError ->{
+            LaunchedEffect(listBrgUIstate.errorMessage) {
+                listBrgUIstate.errorMessage?.let { message ->
+                    coroutineScope.launch {
+                        snackbarHostState.showSnackbar(message)
+                    }
+                }
+            }
+        }
+        listBrgUIstate.listBrg.isEmpty() ->{
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                Text(
+                    text = "Tidak ada data Barang",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+        else -> {
+            ListBarang(
+                listBrg = listBrgUIstate.listBrg,
+                onClick = {
+                    onClick(it)
+                    println(it)
+                },
+                modifier = modifier
+            )
+        }
+    }
+}
 
 
 @Composable
