a1ef0f7df6123e0d11b6cfa12f0187279d0c30f5
Muhammadalif1213 <muhammadalif1213@gmail.com>
2024-12-22 22:28:26 +0700

    Menambahkan Class untuk mengelola state UI barang detail,loading, error, dan delete barang.

--- app/src/main/java/com/example/ucp2_075/ui/viewmodel/DetailBrgViewModel.kt
+++ app/src/main/java/com/example/ucp2_075/ui/viewmodel/DetailBrgViewModel.kt
@@ -1,6 +1,64 @@
 package com.example.ucp2_075.ui.viewmodel
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2_075.data.entity.Barang
+import com.example.ucp2_075.repository.RepoBrg
+import com.example.ucp2_075.ui.navigation.DestinasiDetailBrg
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+class DetailBrgViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repoBrg: RepoBrg,
+): ViewModel(){
+    private val _id_Barang: String = checkNotNull(savedStateHandle[DestinasiDetailBrg.ID_BRG])
+
+    val detailBrgUIstate: StateFlow<DetailBrgUiState> = repoBrg.getBrg(_id_Barang)
+        .filterNotNull()
+        .map { DetailBrgUiState(
+            detailBarangUiEvent = it.toDetailBrgUiEvent(),
+            isLoading = false,
+        )
+        }
+        .onStart {
+            emit(DetailBrgUiState(isLoading = true))
+            delay(600)
+        }
+        .catch {
+            emit(
+                DetailBrgUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = DetailBrgUiState(
+                isLoading = true,
+            ),
+        )
+    fun deleteBrg(){
+        detailBrgUIstate.value.detailBarangUiEvent.toBarangEntity().let {
+            viewModelScope.launch {
+                repoBrg.deleteBrg(it)
+            }
+        }
+    }
+}
+
+
 
 data class DetailBrgUiState(
     val detailBarangUiEvent: BarangEvent = BarangEvent(),
