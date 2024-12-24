2c880f9629df8a42beff1c6ac8770b7eaa69baf7
Muhammadalif1213 <muhammadalif1213@gmail.com>
2024-12-24 08:47:16 +0700

    Mengganti nama database menjadi DBToko

--- app/src/main/java/com/example/ucp2_075/data/database/TokoDatabase.kt
+++ app/src/main/java/com/example/ucp2_075/data/database/TokoDatabase.kt
@@ -24,7 +24,7 @@ abstract class TokoDatabase : RoomDatabase(){
                 Room.databaseBuilder(
                     context,
                     TokoDatabase::class.java,
-                    "TokoDatabase"
+                    "DBToko"
                 )
                     .build().also { Instance = it }
             })
